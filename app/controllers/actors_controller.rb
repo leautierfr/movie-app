da3e443d17a8ee10b3439fe4153527476c3e0337
FR <leautier.fr@gmail.com>
2022-07-07 10:51:55 -0400

    adds restful routes for the actor and movie models

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,6 +1,38 @@
 class ActorsController < ApplicationController
-  def one_actor
-    one_actor = Actor.find_by(id: params["id"])
-    render json: one_actor.as_json
+  def show 
+    actor = Actor.find_by(id: params[:id])
+    render json: actor.as_json
   end
+
+  def index 
+    actors = Actor.all
+    render json: actors.as_json
+  end
+
+  def create
+    actor = Actor.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      known_for: params[:known_for]
+    )
+    actor.save
+    render json: actor.as_json
+  end
+
+  def update
+    actor = Actor.find_by(id: params[:id])
+    actor.first_name = params[:first_name] || actor.first_name
+    actor.last_name = params[:last_name] || actor.last_name
+    actor.known_for = params[:known_for] || actor.known_for
+    actor.save
+    render json: actor.as_json
+  end
+
+  def destroy 
+    actor = Actor.find_by(id: params[:id])
+    actor.destroy
+    render json: {message: "actor destroyed"}
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "one_actor/:id", controller: "actors", action: "one_actor"
-  get "movie/:id" => "movies#show"
-  get "movies" => "movies#index"
-  get "starwars" =>"movies#starwars"
-  get "lotr" =>"movies#lotr"
+  get "/movie/:id" => "movies#show"
+  get "/movies" => "movies#index"
+  post "/movies" => "movies#create"
+  patch "/movie/:id" => "movies#update"
+  delete "/movie/:id" => "movies#destroy"
+  get "/actor:id" => "actors#show"
+  get "/actors" => "actors#index"
+  post "/actors" => "actors#create"
+  patch "/actor/:id" => "actors#update"
+  delete "/actor/:id" => "actors#destroy"
 end
